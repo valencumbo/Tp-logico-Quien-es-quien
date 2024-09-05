bd8686796040940901e17c90292b3ddaee0af0d2
ivan petrone <ivanpetroneooo@gmail.com>
2024-09-05 11:29:15 -0300

    Primera version del punto 1 y 2a

---
+++ TpLogico.pl
@@ -0,0 +1,30 @@
+persona(samuel, lentes(negros)).
+persona(samuel, pelo(blanco,lacio)).
+persona(samuel, calvo).
+persona(samuel, boca(chica)).
+persona(samuel, cara(puntuda)).
+persona(samuel,ojos(marrones)).
+persona(samuel, nariz(chica)).
+persona(pepe, lentes(rojos)).
+persona(pepe, pelo(rubio,rulos)).
+persona(pepe, boca(chica)).
+persona(pepe, cara(redonda)).
+persona(pepe, ojos(marrones)).
+persona(pepe, nariz(chica)).
+carta(rojo, samuel).
+carta(azul, pepe).
+contrincante(rojo, azul).
+contrincante(azul, rojo).
+
+
+esCalvo(Persona):-persona(Persona,calvo).
+esRubio(Persona):-persona(Persona,pelo(rubio,_)).
+tieneLentes(Persona):-persona(Persona,boca(grande)),persona(Persona,nariz(chica)).
+
+tieneOjosMarrones(Persona):-persona(Persona,pelo(negro,_)).
+tieneOjosMarrones(Persona):-persona(Persona,pelo(castaño,_)).
+
+
+
+
+
