fa4e10b4da5b27a24145da84c6b9c3c06cbd78a3
ivan petrone <ivanpetroneooo@gmail.com>
2024-09-06 23:01:06 -0300

    Archivo del tp agregado con include y arreglo de algunos problemas con los test

--- Test.pl
+++ Test.pl
@@ -1,3 +1,20 @@
+:-include('TpLogico.pl').
 
+:-begin_tests(calvos).
+test("la persona es calva"):-esCalvo(samuel).
+test("la persona no es calva",fail):-esCalvo(pepe).
+:-end_tests(calvos).
 
+:-begin_tests(rubios).
+test("la persona es rubia"):-esRubio(pepe).
+test("la persona no es rubia",fail):-esRubio(samuel).
+:-end_tests(rubios).
+
+:-begin_tests(lentes).
+test("la persona tiene lentes",set(Persona = [samuel, pepe])):-tieneLentes(Persona).
+:-end_tests(lentes).
+
+:-begin_tests(ojosmarrones).
+test("la persona tiene ojos marrones",set(Persona = [samuel, pepe])):-tieneOjosMarrones(Persona).
+:- end_tests(ojosmarrones).
 
