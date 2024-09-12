87ecb349a4d3c829046d03def773787371d001ca
MauroLegaspi <maurolegaspi110400.ml@gmail.com>
2024-09-11 21:50:21 -0300

    Update Test.pl
    Agrego test del punto 3.

--- Test.pl
+++ Test.pl
@@ -12,9 +12,20 @@ test("la persona no es rubia",fail):-esRubio(samuel).
 
 :-begin_tests(lentes).
 test("la persona tiene lentes",set(Persona = [samuel, pepe])):-tieneLentes(Persona).
+test("la persona no tiene lentes",fail):-not(tieneLentes(samuel)),not(tieneLentes(pepe)).
 :-end_tests(lentes).
 
 :-begin_tests(ojosmarrones).
 test("la persona tiene ojos marrones",set(Persona = [samuel, pepe])):-tieneOjosMarrones(Persona).
+test("la persona no tiene ojos marrones",fail):-not(tieneOjosMarrones(samuel)),not(tieneOjosMarrones(pepe)).
 :- end_tests(ojosmarrones).
 
+:-begin_tets(cararedonda).
+test("la persona tiene la cara redonda"):-not(tieneCaraRedonda(samuel)), not(tieneCaraRedonda(pepe)).
+test("la persona no tiene la cara redonda",fail):-tieneCaraRedonda(samuel),tieneCaraRedonda(pepe).
+:- end_tests(cararedonda).
+
+:-begin_tests(rubiosbocachica).
+test("todos los rubios de boca chica"):-rubiosBocaChica([pepe]).
+test("todos los que no rubios de boca chica",fail9:-rubiosBocaChica([samuel]).
+:-end_tests(rubiosbocachica).
