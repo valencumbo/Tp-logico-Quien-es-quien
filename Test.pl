e61eb8eaf65bfa9e7ff272d48bb05cf20cbfc1b1
JuanIAlegre <juanignacioalegre1@gmail.com>
2024-09-13 16:42:10 -0300

    Update Test.pl
    Cambio Por error de tipeo

--- Test.pl
+++ Test.pl
@@ -20,12 +20,12 @@ test("la persona tiene ojos marrones",set(Persona = [samuel, pepe])):-tieneOjosM
 test("la persona no tiene ojos marrones",fail):-not(tieneOjosMarrones(samuel)),not(tieneOjosMarrones(pepe)).
 :- end_tests(ojosmarrones).
 
-:-begin_tets(cararedonda).
+:-begin_tests(cararedonda).
 test("la persona tiene la cara redonda"):-not(tieneCaraRedonda(samuel)), not(tieneCaraRedonda(pepe)).
 test("la persona no tiene la cara redonda",fail):-tieneCaraRedonda(samuel),tieneCaraRedonda(pepe).
 :- end_tests(cararedonda).
 
 :-begin_tests(rubiosbocachica).
 test("todos los rubios de boca chica"):-rubiosBocaChica([pepe]).
-test("todos los que no rubios de boca chica",fail9:-rubiosBocaChica([samuel]).
+test("todos los que no rubios de boca chica",fail):-rubiosBocaChica([samuel]).
 :-end_tests(rubiosbocachica).
