f999eee5bf7c337f7edb76d183dfb7e723fc18a6
valencumbo <122327446+valencumbo@users.noreply.github.com>
2024-09-13 19:25:51 -0300

    Update TpLogico.pl
    incorporacion del punto 4 y 5 del trabajo
    correccion en la funcion tieneLentes, que se encontraba declarada 2 veces, haciendo que la segunda sea redundante

--- TpLogico.pl
+++ TpLogico.pl
@@ -1,32 +1,45 @@
-persona(samuel,lentes(negros)).
-persona(samuel,pelo(blanco,lacio)).
-persona(samuel,calvo).
-persona(samuel,boca(chica)).
-persona(samuel,cara(puntuda)).
-persona(samuel,ojos(marrones)).
-persona(samuel,nariz(chica)).
-persona(pepe,lentes(rojos)).
-persona(pepe,pelo(rubio,rulos)).
-persona(pepe,boca(chica)).
-persona(pepe,cara(puntuda)).
-persona(pepe,ojos(marrones)).
-persona(pepe,nariz(chica)).
-carta(rojo,samuel).
-carta(azul,pepe).
-contrincante(rojo,azul).
-contrincante(azul,rojo).
+persona(samuel, lentes(negros)).
+persona(samuel, pelo(blanco, lacio)).
+persona(samuel, calvo).
+persona(samuel, boca(chica)).
+persona(samuel, cara(puntuda)).
+persona(samuel, ojos(marrones)).
+persona(samuel, nariz(chica)).
+persona(pepe, lentes(rojos)).
+persona(pepe, pelo(rubio, rulos)).
+persona(pepe, boca(chica)).
+persona(pepe, cara(puntuda)).
+persona(pepe, ojos(marrones)).
+persona(pepe, nariz(chica)).
+carta(rojo, samuel).
+carta(azul, pepe).
+contrincante(rojo, azul).
+contrincante(azul, rojo).
 
-esCalvo(Persona):-persona(Persona,calvo).
-esRubio(Persona):-persona(Persona,pelo(rubio,_)).
+esCalvo(Persona) :- persona(Persona, calvo).
+esRubio(Persona) :- persona(Persona, pelo(rubio, _)).
 
-tieneLentes(Persona):-persona(Persona,lentes(_)).
-tieneLentes(Persona):-persona(Persona,lentes(_)).
+tieneLentes(Persona) :- persona(Persona, lentes(_)).
+tieneLentes(Persona) :- persona(Persona, boca(grande)), persona(Persona, nariz(chica)).
+tieneOjosMarrones(Persona) :- persona(Persona, ojos(marrones)).
+tieneOjosMarrones(Persona) :- persona(Persona, pelo(negro, _)).
+tieneOjosMarrones(Persona) :- persona(Persona, pelo(castaño, _)).
+tieneCaraRedonda(Persona) :- not(persona(Persona, cara(puntuda))).
 
-tieneLentes(Persona):-persona(Persona,boca(grande)),persona(Persona,nariz(chica)).
-tieneOjosMarrones(Persona):-persona(Persona,ojos(marrones)).
-tieneOjosMarrones(Persona):-persona(Persona,pelo(negro,_)).
-tieneOjosMarrones(Persona):-persona(Persona,pelo(castaño,_)).
-tieneCaraRedonda(Persona):- not(persona(Persona, cara(puntuda))).
+tieneBocaChica(Persona) :- persona(Persona, boca(chica)).
 
-tieneBocaChica(Persona):-persona(Persona,boca(chica)).
-rubiosBocaChica(Personas):-findall(Persona,(esRubio(Persona),tieneBocaChica(Persona)),Personas).
+rubiosBocaChica(Personas) :-
+    findall(Persona, (esRubio(Persona), tieneBocaChica(Persona)), Personas).
+
+cumple_pistas(Persona, Pistas) :-
+    forall(member(Pista, Pistas), persona(Persona, Pista)).
+
+ganando(Jugador, PistasJugador, PistasContrincante) :-
+    contrincante(Jugador, Contrincante),
+    cantidad_que_cumple(Jugador, PistasJugador, CantidadJugador),
+    cantidad_que_cumple(Contrincante, PistasContrincante, CantidadContrincante),
+    CantidadJugador < CantidadContrincante.
+
+cantidad_que_cumple(Jugador, Pistas, Cantidad) :-
+    findall(Persona, cumple_pistas(Persona, Pistas), Personas),
+    length(Personas, Cantidad).
